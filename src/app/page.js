0b382f4b007f94e2c92d1f03fe4c64822842b02c
William Kosso <wkosso@iu.edu>
2024-10-16 16:42:18 -0400

    Changed the import to PageStyles Made the styling for the homepage banner.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
-import styles from "./page.module.css";
+import Pagestyles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+    <div className={Pagestyles.homecontainerBanner}>
+      <div className={Pagestyles.homeimgcontainerBanner}>
+        <div className={Pagestyles.homebannerText}>
+<div> Welcome to Wolf Park</div>
+ <div>Discover, protect, and celebrate wolves.</div>
+ </div>
+    </div>
+    </div>
     </main>
   );
 }
