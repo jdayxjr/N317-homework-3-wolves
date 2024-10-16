a5667cf8c4991c7b3442f0acc0f9339950a1f65b
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2024-10-16 14:48:54 -0400

    explore banner image added and text centered some csss styling added to explore

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,10 +1,14 @@
+import exploreStyles from './explore.module.css';
 export default function Explore() {
     return (
         <main>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={exploreStyles.imageBanner}>
+                <div className={exploreStyles.bannerText}><h1>Wolf Wild Life</h1>
+                    <h2>exlpore the wilderness among us...</h2>
+                </div>
+
+            </div>
+
         </main>
     );
 }
