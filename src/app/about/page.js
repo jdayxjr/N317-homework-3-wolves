e3311baa8407f42eb7bf5d22fb73aea927690321
William Kosso <wkosso@iu.edu>
2024-10-16 14:46:37 -0400

    finished the banner for the about and started adding text to it

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+import aboutStyles from '@/app/about/about.module.css'
+
 export default function About() {
 
 
@@ -6,10 +8,12 @@ export default function About() {
 
     return (
 <main>
-    <div>
+    <div className={aboutStyles.aboutContainer}>
 
-        <div>About US</div>
-        <div>Welcome to Wolf Park, where the wild spirit of wolves comes to life! Our park is dedicated to the conservation, education, and appreciation of these magnificent animals. Nestled in nature's beauty, we provide a sanctuary for wolves and a unique opportunity for visitors to learn about their importance in the ecosystem.</div>
+<div className={aboutStyles.aboutHeading}>About US</div>
+
+        <div className={aboutStyles.aboutmainText}>
+       Welcome to Wolf Park, where the wild spirit of wolves comes to life! Our park is dedicated to the conservation, education, and appreciation of these magnificent animals. Nestled in nature's beauty, we provide a sanctuary for wolves and a unique opportunity for visitors to learn about their importance in the ecosystem.</div>
     </div>
         </main>
     );
