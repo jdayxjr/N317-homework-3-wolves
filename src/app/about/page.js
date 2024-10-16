a964e7a24fa9ec458f9a5372569e7a6c7595781c
William Kosso <wkosso@iu.edu>
2024-10-16 14:26:58 -0400

    Started adding about text and started working on the css

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,16 @@
 export default function About() {
+
+
+
+
+
     return (
-        <main>
-            <h1>About</h1>
+<main>
+    <div>
+
+        <div>About US</div>
+        <div>Welcome to Wolf Park, where the wild spirit of wolves comes to life! Our park is dedicated to the conservation, education, and appreciation of these magnificent animals. Nestled in nature's beauty, we provide a sanctuary for wolves and a unique opportunity for visitors to learn about their importance in the ecosystem.</div>
+    </div>
         </main>
     );
 }
