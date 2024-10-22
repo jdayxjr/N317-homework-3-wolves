c45d85ef3da45567f0e250fe1d1d0c7d1c116cf2
William Kosso <wkosso@iu.edu>
2024-10-22 16:01:32 -0400

    added a section to the homepage with images and text.

--- src/app/explore/index.js
+++ src/app/explore/index.js
@@ -14,7 +14,7 @@ export default function ExploreImage({ title, description, imageUrl, extraInfo }
     return (
         <div className={exploreStyles.exploreBox}>
             <Image
-                src={imageUrl} // Path relative to the 'public' folder
+                src={imageUrl} 
                 alt="Explore the wild"
                 width={300}
                 height={200}
@@ -22,15 +22,18 @@ export default function ExploreImage({ title, description, imageUrl, extraInfo }
             <h3>{title}</h3>
             <p>{description}</p>
 
-            {/* Conditionally render extra information if isExpanded is true */}
+           
             {isExpanded && (
                 <p className={exploreStyles.extraInfo}>{extraInfo}</p>
             )}
 
-            {/* Button toggles between "Learn More" and "Show Less" */}
+            
             <button onClick={handleToggle} className={exploreStyles.learnMoreButton}>
                 {isExpanded ? "Show Less" : "Learn More"}
             </button>
         </div>
+
+
+
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,18 @@ export default function Home() {
  </div>
     </div>
     </div>
+
+    <div className={Pagestyles.Firstsectioncontainer}>
+    <div  className={Pagestyles.homeminiImage} ></div>
+
+
+  <div className={Pagestyles.hometextSection}>
+    <p>Step into the wild and experience the magic of wolves up close. At Wolf Park, we provide a safe haven for these majestic animals and offer visitors a unique opportunity to witness their beauty and power. Explore our natural habitats, learn about the wolves’ role in the ecosystem, and be part of our mission to protect these incredible creatures.</p>
+  </div>
+</div>
+
+
+    
     </main>
   );
 }
